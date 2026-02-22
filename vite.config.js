b3d1987753fa04b0a09c6c606a8d947306f9d3a6
lxry-comet <durdentagirowich@gmail.com>
2026-02-22 10:53:39 +0100

    updateUrl

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
+//todo: https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/airplane-model-store2-stylization2/', //! <-- ім'я репозиторію
+  //! Налаштування Аліасів для абсолютних шляхів імпортів
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
